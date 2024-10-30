c0d384715732ce880761c1f6bf10e57ffcfd3543
chenjunhao <1176470910@qq.com>
2024-10-30 14:51:36 +0800

    111

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,40 +1,31 @@
 <script setup lang="ts">
+import EventCategoryOrganizer from '@/components/EventCategoryOrganizer.vue';
 import EventCard from '@/components/EventCard.vue'
 import type { Event } from '@/type'
-import { ref } from 'vue'
-const events = ref<Event[]>([
-  {
-    id: 5928101,
-    category: 'animal welfare',
-    title: 'Cat Adoption Day',
-    description: 'Find your new feline friend at this event.',
-    location: 'Meow Town',
-    date: 'January 28, 2022',
-    time: '12:00',
-    petsAllowed: true,
-    organizer: 'Kat Laydee'
-  },
-  {
-    id: 4582797,
-    category: 'food',
-    title: 'Community Gardening',
-    description: 'Join us as we tend to the community edible plants.',
-    location: 'Flora City',
-    date: 'March 14, 2022',
-    time: '10:00',
-    petsAllowed: true,
-    organizer: 'Fern Pollin'
-  },
-  {
-    id: 8419988,
-    category: 'sustainability',    title: 'Beach Cleanup',    description: 'Help pick up trash along the shore.',    location: 'Playa Del Carmen',    date: 'July 22, 2022',    time: '11:00',    petsAllowed: false,    organizer: 'Carey Wales'  }])
+import { ref, onMounted } from 'vue'
+import EventService from '@/services/EventService'
+
+const events = ref<Event[]>()
+
+onMounted(() => {
+  EventService.getEvents()
+    .then((response) => {
+      events.value = response.data
+    })
+    .catch((error) => {
+      console.error('There was an error!', error)
+    })
+})
 </script>
 
 <template>
 <h1>Events For Good</h1>
   <!-- new element -->
 <div class="events">
-  <EventCard v-for="event in events" :key="event.id" :event="event" />
+  <div v-for="event in events" :key="event.id" class="eventcontainer">
+      <EventCard :event="event" />
+      <EventCategoryOrganizer :event="event" category="event.category" :organizer="event.organizer" />
+  </div>
 </div>
 </template>
 
@@ -43,5 +34,15 @@ const events = ref<Event[]>([
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.eventcontainer{
+  display: flex;
+  flex-direction: row; /* 使内容水平排列 */
+  justify-content: space-between; /* 使内容两端对齐 */
+  width: 100%; /* 根据需要调整宽度 */
+  max-width: 600px; /* 设置最大宽度 */
+  border: 1px solid #ccc; /* 可选：添加边框 */
+  padding: 10px; /* 可选：添加内边距 */
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* 可选：添加阴影 */
 }
 </style>
